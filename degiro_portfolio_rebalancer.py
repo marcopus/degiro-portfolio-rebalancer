5c79ceb70186300db66db81563214513e276c24e
Marco Marra <marrez89@gmail.com>
2022-09-21 20:43:27 +0200

    Format code with black

--- degiro_portfolio_rebalancer.py
+++ degiro_portfolio_rebalancer.py
@@ -7,38 +7,32 @@ import sys
 
 import degiro_connector.core.helpers.pb_handler as pb_handler
 from degiro_connector.trading.api import API as TradingAPI
-from degiro_connector.trading.models.trading_pb2 import Credentials, ProductsInfo, TransactionsHistory, Update
+from degiro_connector.trading.models.trading_pb2 import (
+    Credentials,
+    ProductsInfo,
+    TransactionsHistory,
+    Update,
+)
 
 TRANSACTION_COSTS = 2.50
 
-def fetch_account_number(
-    username: str,
-    password: str,
-    totp_secret_key: str
-) -> int:
+
+def fetch_account_number(username: str, password: str, totp_secret_key: str) -> int:
     """
     Attempts to fetch the account number for the given credentials.
     Returns an integer (account number) or raises a ValueError.
     """
-    trading_api = setup_connection(
-        None,
-        username,
-        password,
-        totp_secret_key
-    )
+    trading_api = setup_connection(None, username, password, totp_secret_key)
 
     # fetch the account number
     try:
-        return trading_api.get_client_details()['data']['intAccount']
+        return trading_api.get_client_details()["data"]["intAccount"]
     except:
         raise ValueError("Unable to fetch the account number.")
 
+
 def setup_connection(
-    int_account: int,
-    username: str,
-    password: str,
-    totp_secret_key: str,
-    **kwargs
+    int_account: int, username: str, password: str, totp_secret_key: str, **kwargs
 ) -> TradingAPI:
     """
     Establishes a connection using the degiro-connector TradingAPI and the current credentials.
@@ -49,7 +43,7 @@ def setup_connection(
         int_account=int_account,
         username=username,
         password=password,
-        totp_secret_key=totp_secret_key
+        totp_secret_key=totp_secret_key,
     )
 
     # SETUP TRADING API
@@ -63,27 +57,31 @@ def setup_connection(
 
     return trading_api
 
+
 def validate_credentials(config_dict: dict) -> dict:
     """
     Validates the provided credentials, optionally fetches the account number and returns the
     updated credentials. Raises ValueError for any missing data.
     """
-    for key in ['username', 'password']:
+    for key in ["username", "password"]:
         if key not in config_dict.keys():
-            raise ValueError(f'Missing {key} in credentials provided.')
-    
-    if not config_dict.get('int_account'):
+            raise ValueError(f"Missing {key} in credentials provided.")
+
+    if not config_dict.get("int_account"):
         # account number is unknown; fetch it using current config credentials
-        config_dict['int_account'] = fetch_account_number(
-            config_dict.get('username'),
-            config_dict.get('password'),
-            config_dict.get('totp_secret_key')
+        config_dict["int_account"] = fetch_account_number(
+            config_dict.get("username"),
+            config_dict.get("password"),
+            config_dict.get("totp_secret_key"),
         )
-        print("Your account number:", config_dict['int_account'])
+        print("Your account number:", config_dict["int_account"])
 
     return config_dict
 
-def read_configuration_from_file(account_name: str, config_dirpath: str="config") -> dict:
+
+def read_configuration_from_file(
+    account_name: str, config_dirpath: str = "config"
+) -> dict:
     """
     Reads a json configuration file for the account named `account_name`.
     Returns a configuration dictionary and the relative path path to the json file.
@@ -98,8 +96,9 @@ def read_configuration_from_file(account_name: str, config_dirpath: str="config"
     except json.decoder.JSONDecodeError as e:
         print(f'ERROR: unable to decode json file "{account_name}".json')
         sys.exit(1)
-    
+
     return config_dict
+
 
 def rebalance(config: dict):
     """
@@ -110,8 +109,12 @@ def rebalance(config: dict):
     trading_api = setup_connection(**config)
 
     # SETUP REQUEST TRANSACTIONS
-    reference_date_from = dp.parse(config.get('reference_date_from'), settings={'DATE_ORDER': 'YMD'})
-    reference_date_to = dp.parse(config.get('reference_date_to'), settings={'DATE_ORDER': 'YMD'})
+    reference_date_from = dp.parse(
+        config.get("reference_date_from"), settings={"DATE_ORDER": "YMD"}
+    )
+    reference_date_to = dp.parse(
+        config.get("reference_date_to"), settings={"DATE_ORDER": "YMD"}
+    )
     from_date = TransactionsHistory.Request.Date(
         year=reference_date_from.year,
         month=reference_date_from.month,
@@ -137,10 +140,11 @@ def rebalance(config: dict):
     transactions_df = pd.DataFrame(
         [dict(transaction) for transaction in transactions_history.values]
     )
-    transactions_df = transactions_df.groupby('productId').agg({'quantity': 'sum', 'total': 'sum'})
+    transactions_df = transactions_df.groupby("productId").agg(
+        {"quantity": "sum", "total": "sum"}
+    )
     transactions_df.index = transactions_df.index.astype(int).astype(str)
-    transactions_df['ratio'] = transactions_df.total/transactions_df.total.sum()
-
+    transactions_df["ratio"] = transactions_df.total / transactions_df.total.sum()
 
     # SETUP REQUEST PRODUCTS INFO
     request = ProductsInfo.Request()
@@ -151,8 +155,9 @@ def rebalance(config: dict):
         request=request,
         raw=True,
     )
-    products_info_df = pd.DataFrame(products_info['data']).transpose()[['name', 'isin', 'symbol']]
-
+    products_info_df = pd.DataFrame(products_info["data"]).transpose()[
+        ["name", "isin", "symbol"]
+    ]
 
     # SETUP REQUEST PORTFOLIO
     request_list = Update.RequestList()
@@ -167,56 +172,70 @@ def rebalance(config: dict):
     update_dict = pb_handler.message_to_dict(message=update)
 
     # DISPLAY DATA PORTFOLIO
-    if 'portfolio' not in update_dict:
+    if "portfolio" not in update_dict:
         raise Exception("No portfolio data!")
 
-    portfolio_df = pd.DataFrame(update_dict['portfolio']['values'])
+    portfolio_df = pd.DataFrame(update_dict["portfolio"]["values"])
     portfolio_df
 
-
     # PREPARE PORTFOLIO DATA
-    cash = portfolio_df.loc[portfolio_df['positionType'] == 'CASH',  'value'].sum()
-    p = portfolio_df.loc[portfolio_df['positionType'] == 'PRODUCT'][['id', 'value', 'price']].set_index('id')
-    p['ratio'] = p.value / (cash + p.value.sum())
-    p = p.merge(transactions_df.ratio, how='inner', left_index=True, right_index=True, suffixes=('_current', '_initial'))
+    cash = portfolio_df.loc[portfolio_df["positionType"] == "CASH", "value"].sum()
+    p = portfolio_df.loc[portfolio_df["positionType"] == "PRODUCT"][
+        ["id", "value", "price"]
+    ].set_index("id")
+    p["ratio"] = p.value / (cash + p.value.sum())
+    p = p.merge(
+        transactions_df.ratio,
+        how="inner",
+        left_index=True,
+        right_index=True,
+        suffixes=("_current", "_initial"),
+    )
     p = p.join(products_info_df)
 
     # CALCULATE PORTFOLIO REBALANCING
-    p['buy/sell %'] = (1 - p.ratio_current / p.ratio_initial) * 100
-    p['buy/sell'] = p['buy/sell %'] / 100 * p.value
-    p['buy/sell units'] = (p['buy/sell'] / p.price).round().astype(int)
-    idx_min = p['buy/sell'].idxmin()
+    p["buy/sell %"] = (1 - p.ratio_current / p.ratio_initial) * 100
+    p["buy/sell"] = p["buy/sell %"] / 100 * p.value
+    p["buy/sell units"] = (p["buy/sell"] / p.price).round().astype(int)
+    idx_min = p["buy/sell"].idxmin()
     new_portfolio_value = p.value[idx_min] / p.ratio_initial[idx_min]
-    p['buy-only'] = new_portfolio_value * p.ratio_initial - p.value
-    p['buy-only units'] = (p['buy-only'] / p.price).round().astype(int)
-    amount_transaction_buy_sell = -(p['buy/sell units'] * p.price).sum()
-    amount_transaction_buy_only = -(p['buy-only units'] * p.price).sum()
-    fees_buy_sell = -TRANSACTION_COSTS * (p['buy/sell units']!=0).sum()
-    fees_buy_only = -TRANSACTION_COSTS * (p['buy-only units']!=0).sum()
+    p["buy-only"] = new_portfolio_value * p.ratio_initial - p.value
+    p["buy-only units"] = (p["buy-only"] / p.price).round().astype(int)
+    amount_transaction_buy_sell = -(p["buy/sell units"] * p.price).sum()
+    amount_transaction_buy_only = -(p["buy-only units"] * p.price).sum()
+    fees_buy_sell = -TRANSACTION_COSTS * (p["buy/sell units"] != 0).sum()
+    fees_buy_only = -TRANSACTION_COSTS * (p["buy-only units"] != 0).sum()
     print(p)
     print(
-        "\n".join((
-            "Amount currently available to trade: €{:.2f}".format(cash),
-            "Deposit needed for buy/sell rebalancing: €{:.2f}".format(
-                -min(0, amount_transaction_buy_sell + fees_buy_sell + cash)
-            ),
-            "Deposit needed for buy-only rebalancing: €{:.2f}".format(
-                -min(0, amount_transaction_buy_only + fees_buy_only + cash)
-            ),
-        ))
+        "\n".join(
+            (
+                "Amount currently available to trade: €{:.2f}".format(cash),
+                "Deposit needed for buy/sell rebalancing: €{:.2f}".format(
+                    -min(0, amount_transaction_buy_sell + fees_buy_sell + cash)
+                ),
+                "Deposit needed for buy-only rebalancing: €{:.2f}".format(
+                    -min(0, amount_transaction_buy_only + fees_buy_only + cash)
+                ),
+            )
+        )
     )
 
-if __name__ == '__main__':
-    pd.options.display.float_format = '{:.2f}'.format
+
+if __name__ == "__main__":
+    pd.options.display.float_format = "{:.2f}".format
 
     # SETUP LOGGING LEVEL
     logging.basicConfig(level=logging.ERROR)
 
     # SETUP CONFIG DICT
     if len(sys.argv) < 2:
-        print("Missing account name", "Usage: python degiro_portfolio_rebalancer.py <account_name>", sep="\n")
+        print(
+            "Missing account name",
+            "Usage: python degiro_portfolio_rebalancer.py <account_name>",
+            sep="\n",
+        )
         sys.exit(64)
-    
+
     # READ CONFIGURATION
     config = read_configuration_from_file(sys.argv[1])
 
--- tests/test_degiro_portfolio_rebalancer.py
+++ tests/test_degiro_portfolio_rebalancer.py
@@ -10,45 +10,43 @@ class TestSetupConnection(unittest.TestCase):
         self.password = "password"
         self.totp_secret_key = "totp_secret_key"
 
-    @mock.patch('degiro_portfolio_rebalancer.Credentials')
-    @mock.patch('degiro_portfolio_rebalancer.TradingAPI')
-    def test_setup_connection_with_no_account_returns_api(self, MockTradingAPI, MockCredentials):
+    @mock.patch("degiro_portfolio_rebalancer.Credentials")
+    @mock.patch("degiro_portfolio_rebalancer.TradingAPI")
+    def test_setup_connection_with_no_account_returns_api(
+        self, MockTradingAPI, MockCredentials
+    ):
+        api = setup_connection(None, self.username, self.password, self.totp_secret_key)
+        assert api is MockTradingAPI()
+
+    @mock.patch("degiro_portfolio_rebalancer.Credentials")
+    @mock.patch("degiro_portfolio_rebalancer.TradingAPI")
+    def test_setup_connection_with_account_returns_api(
+        self, MockTradingAPI, MockCredentials
+    ):
         api = setup_connection(
-            None,
-            self.username,
-            self.password,
-            self.totp_secret_key
+            1234567, self.username, self.password, self.totp_secret_key
         )
         assert api is MockTradingAPI()
 
-    @mock.patch('degiro_portfolio_rebalancer.Credentials')
-    @mock.patch('degiro_portfolio_rebalancer.TradingAPI')
-    def test_setup_connection_with_account_returns_api(self, MockTradingAPI, MockCredentials):
-        api = setup_connection(
-            1234567,
-            self.username,
-            self.password,
-            self.totp_secret_key
-        )
-        assert api is MockTradingAPI()
-
-    @mock.patch('degiro_portfolio_rebalancer.Credentials')
+    @mock.patch("degiro_portfolio_rebalancer.Credentials")
     def test_validate_credentials_fetches_account_number(self, MockCredentials):
-        with mock.patch('degiro_portfolio_rebalancer.TradingAPI') as MockTradingAPI:
+        with mock.patch("degiro_portfolio_rebalancer.TradingAPI") as MockTradingAPI:
             trading_api = MockTradingAPI.return_value
             client_details = mock.MagicMock(spec_set=dict)
-            client_details.__getitem__.side_effect = lambda name: {'intAccount': 1234567}
+            client_details.__getitem__.side_effect = lambda name: {
+                "intAccount": 1234567
+            }
             trading_api.get_client_details.return_value = client_details
 
             config = validate_credentials(
                 {
-                    'username': self.username,
-                    'password': self.password,
-                    'totp_secret_key': self.totp_secret_key,
+                    "username": self.username,
+                    "password": self.password,
+                    "totp_secret_key": self.totp_secret_key,
                 }
             )
-            self.assertEqual(1234567, config['int_account'])
-        
+            self.assertEqual(1234567, config["int_account"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
